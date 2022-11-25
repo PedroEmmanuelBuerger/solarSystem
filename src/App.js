da737ce5812896bde41d2bade9006fe9db7d1379
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-11-25 17:31:15 -0300

    resolvendo lint

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
-import './style.css';
 
 class App extends React.Component {
   render() {
