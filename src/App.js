4fc8ce9946e62761549d4cef4ede2c9a734a8b33
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-11-25 15:34:05 -0300

    resolvendo requesito 2 e 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <SolarSystem />
+        <Title headline="" />
+      </div>
     );
   }
 }
