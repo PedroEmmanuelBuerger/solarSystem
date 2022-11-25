f6a8e29987994602c3790d18ed04eeae306d510f
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-11-25 16:37:40 -0300

    resolvendo requesito 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map((element) => (<MissionCard
+          key={ element.name }
+          name={ element.name }
+          year={ element.year }
+          country={ element.country }
+          destination={ element.destination }
+        />))}
       </div>
     );
   }
