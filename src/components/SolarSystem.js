11f242aaebfd55c7572444423092ec8b2a04d094
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-11-25 15:52:36 -0300

    resolvendo requesito 5

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -5,7 +5,7 @@ class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <Title headline = 'Planetas'/>
+        <Title headline="Planetas" />
       </div>
     );
   }
