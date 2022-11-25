a8d1f60b57ef8ff0f34ad7cf743c828fdde200e1
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-11-25 16:06:56 -0300

    resolvendo requesito 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((element) => (<PlanetCard
+          key={ element.name }
+          planetName={ element.name }
+          planetImage={ element.image }
+        />))}
       </div>
     );
   }
